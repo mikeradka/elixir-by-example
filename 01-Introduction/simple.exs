09c64e55419189a63f713be722e6ff370f76d370
mikeradka <mikeradka@gmail.com>
2023-10-14 18:52:01 -0400

    Add some operator examples

--- 01-Introduction/simple.exs
+++ 01-Introduction/simple.exs
@@ -1,3 +1,24 @@
 IO.puts("Hello world from Elixir")
-IO.puts("foo" <> "bar")
-IO.puts(5 + 5)
+
+IO.puts("foo" <> "bar") # concatenation
+IO.puts(5 + 5)  # simple math expression
+
+IO.puts(true and false) # boolean operator example
+IO.puts(true or false) # another boolean operator example
+IO.puts(true or raise("This error will never be raised"))
+
+IO.puts(1 || true) # or
+IO.puts(false || 11) # another or
+
+IO.puts(nil && 13) # and
+IO.puts(true && 17) # another and
+
+IO.puts(!true) # not
+IO.puts(!1) # another not
+IO.puts(!nil) # not with nil
+
+IO.puts(1 == 1) # == comparison operator
+IO.puts(1 != 2) # != comparison operator
+IO.puts(1 < 2) # < comparison operator
+IO.puts(1 == 1.0) # less strict structural comparison
+IO.puts(1 === 1.0) # more strict structural comparison
