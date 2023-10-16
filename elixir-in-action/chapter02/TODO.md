f018d1fb440fd721a2da575a764e2914906bc0c8
mikeradka <mikeradka@gmail.com>
2023-10-16 16:00:57 -0400

    add imports_and_aliases.ex

--- elixir-in-action/chapter02/TODO.md
+++ elixir-in-action/chapter02/TODO.md
@@ -4,4 +4,4 @@
 - [x] 2.3.2 Functions (function_defaults.ex)
 - [x] 2.3.3 Function arity (arity_demo.ex, arity_calc.ex)
 - [x] 2.3.4 Function visibility (private_function.ex)
-- [ ] 2.3.5 Imports and aliases
+- [ ] 2.3.5 Imports and aliases (imports_and_aliases.ex)
---
+++ elixir-in-action/chapter02/imports_and_aliases.ex
@@ -0,0 +1,25 @@
+# Imports and aliases
+# Calling functions from another module can sometimes
+# be cumbersome because you need to reference the
+# module name. If your module often calls functions from
+# another module, you can import that other module into
+# your own. Importing a module allows you to call its
+# public functions without prefixing them with the
+# module name:
+
+defmodule MyModule do
+  import IO
+
+  def my_function() do
+    puts "Calling imported function"
+  end
+end
+
+# > iex imports_and_aliase
+# s.ex
+# Erlang/OTP 26 [erts-14.0.2] [source] [64-bit] [smp:16:16] [ds:16:16:10] [async-threads:1] [jit:ns] [dtrace]
+#
+# Interactive Elixir (1.15.7) - press Ctrl+C to exit (type h() ENTER for help)
+# iex(1)> MyModule.my_function()
+# Calling imported function
+# :ok
