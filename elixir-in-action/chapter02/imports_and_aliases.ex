21459c0a6c4a95aea3166ed1d85018258bdba5a7
mikeradka <mikeradka@gmail.com>
2023-10-16 16:22:00 -0400

    add some notes

--- elixir-in-action/chapter02/imports_and_aliases.ex
+++ elixir-in-action/chapter02/imports_and_aliases.ex
@@ -22,3 +22,26 @@ end
 # iex(1)> MyModule.my_function()
 # Calling imported function
 # :ok
+
+# Another construct, 'alias', makes it possible to reference
+# a module under a different name:
+
+defmodule AnotherModule do
+  alias IO, as: MyIO  # creates an alias for IO
+
+  def another_function do
+    MyIO.puts("Calling imported function.") # calls a function using the alias
+  end
+end
+
+# > iex imports_and_aliases.ex                 (base)
+# Erlang/OTP 26 [erts-14.0.2] [source] [64-bit] [smp:16:16] [ds:16:16:10] [async-threads:1] [jit:ns] [dtrace]
+#
+# Interactive Elixir (1.15.7) - press Ctrl+C to exit (type h() ENTER for help)
+# iex(1)> AnotherModule.another_function()
+# Calling imported function.
+# :ok
+
+# Aliases can be useful if a module has a long name.
+# Aliases can help you reduce some noise, especially if you
+# call functions from a long-named module many times.
