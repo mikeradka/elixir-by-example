a7e4198c784943493ed0938f60f18a2ddc871786
mikeradka <mikeradka@gmail.com>
2023-10-22 08:38:51 -0400

    Add boolean atom operator examples

--- elixir-in-action/chapter02/atoms.ex
+++ elixir-in-action/chapter02/atoms.ex
@@ -39,3 +39,69 @@ variable = AnAtom
 string = "variable = AnAtom"
 pad = String.pad_leading(": ", 7)
 IO.puts(string <> pad <> to_string(variable))
+
+# Remember that aliases can also be used to give alternate
+# names to modules
+#
+# iex(3)> alias IO, as: MyIO
+#
+# iex(4)> MyIO.puts("Hello!")
+# Hello!”
+#
+# When you write alias IO, as: MyIO, you instruct the elixir
+# compiler to transform MyIO into IO. Resolving this further,
+# the final result emitted in the generated binary is :Elixir.IO.
+# Therefore, with an alias set up, the following also holds:
+#
+# iex(5)> MyIO == Elixir.IO
+# true
+#
+# Aliases support the proper resolution of modules.
+#
+# ATOMS AS BOOLEANS
+# Elixir doesn't have a dedicated Boolean type. Instead, the
+# atoms :true and :false are used. As syntactic sugar, Elixir
+# allows you to reference these atoms without the starting
+# colon character:
+#
+# iex(1)> :true == true
+# true
+# iex(2)> true
+# true
+true_atom = :true == true
+true_atom_str = ":true == true"
+pad = String.pad_leading(": ", 11)
+IO.puts(true_atom_str <> pad <> to_string(true_atom))
+
+# A boolean is just an atom that has a value of true or false
+# The term Boolean is still used in Elixir to denote an atom
+# that has a value of either :true or :false. The standard
+# logical operators work with Boolean atoms:
+# iex(1)> true and false
+# false
+# iex(2)> false or true
+# true
+# iex(3)> not false
+# true
+# iex(4)> not :an_atom_other_than_true_or_false
+# ** (ArgumentError) argument error
+#     :erlang.not(:an_atom_other_than_true_or_false)
+#     iex:4: (file)
+#
+# AND Boolean operator example
+andd = true and false
+andd_str = "true and false"
+pad = String.pad_leading(": ", 10)
+IO.puts(andd_str <> pad <> to_string(andd))
+
+# OR Boolean operator example
+orr = false or true
+orr_str = "false or true"
+pad = String.pad_leading(": ", 11)
+IO.puts(orr_str <> pad <> to_string(orr))
+
+# NOT Boolean operator example
+nott = not false
+nott_str = "not false"
+pad = String.pad_leading(": ", 15)
+IO.puts(nott_str <> pad <> to_string(nott_str))
