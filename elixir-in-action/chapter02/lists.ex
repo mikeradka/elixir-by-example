25fb6806e7bfefe26b9acdc18c7b5c70e053cfe3
mikeradka <mikeradka@gmail.com>
2023-10-23 11:54:09 -0400

    Add replace_at example

--- elixir-in-action/chapter02/lists.ex
+++ elixir-in-action/chapter02/lists.ex
@@ -20,7 +20,7 @@ end
 prime_numbers = [2, 3, 5, 7]
 expr_str = "prime_numbers = [2, 3, 5, 7]"
 prime_numbers_insp = inspect(prime_numbers)
-Print.print_expression(expr_str, prime_numbers_insp, 0)
+Print.print_expression(expr_str, prime_numbers_insp, 28)
 
 # Lists may look like arrays, but they work like singly linked
 # lists. To do something with the list, you have to traverse it.
@@ -31,7 +31,7 @@ Print.print_expression(expr_str, prime_numbers_insp, 0)
 # 4
 expr_str = "length(prime_numbers)"
 expr = length(prime_numbers)
-Print.print_expression(expr_str, expr, 10)
+Print.print_expression(expr_str, expr, 35)
 
 # List utility functions
 # There are many operations you can do with lists. For a
@@ -46,7 +46,7 @@ Print.print_expression(expr_str, expr, 10)
 # 7
 expr_str = "Enum.at(prime_numbers, 3)"
 expr = Enum.at(prime_numbers, 3)
-Print.print_expression(expr_str, expr, 6)
+Print.print_expression(expr_str, expr, 31)
 
 # Enum.at is another O(n) operation: it iterates from the
 # beginning of the list to the desired element. Lists are
@@ -63,12 +63,31 @@ Print.print_expression(expr_str, expr, 6)
 # false
 expr = 5 in prime_numbers
 expr_str = "5 in prime_numbers"
-Print.print_expression(expr_str, expr, 13)
+Print.print_expression(expr_str, expr, 38)
 
 expr = 4 in prime_numbers
 expr_str = "4 in prime_numbers"
-Print.print_expression(expr_str, expr, 13)
+Print.print_expression(expr_str, expr, 38)
 
 # To manipulate lists, you can use functions from the List
 # module. For example, List.replace_at/3 modifies the
 # element at a certain position:
+#
+# iex(6)> List.replace_at(prime_numbers, 0, 11)
+# [11, 3, 5, 7]
+#
+# As was the case with tuples, the modifier doesn't mutate the
+# variable, but returns the modified version of it, which you
+# need to store to another variable:
+#
+# iex(7)> new_primes = List.replace_at(prime_numbers, 0, 11)
+# [11, 3, 5, 7]
+#
+# Or you can rebind to the same one:
+#
+# iex(8)> prime_numbers = List.replace_at(prime_numbers, 0, 11)
+# [11, 3, 5, 7]
+prime_numbers = List.replace_at(prime_numbers, 0, 11)
+expr = inspect(prime_numbers)
+expr_str = "prime_numbers = List.replace_at(prime_numbers, 0, 11)"
+Print.print_expression(expr_str, expr, 0)
