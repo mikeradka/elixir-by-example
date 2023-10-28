d08c58a92ce0098e66ce009ffd74a7ce8999f441
mikeradka <mikeradka@gmail.com>
2023-10-27 20:11:00 -0400

    Add maps.ex

---
+++ elixir-in-action/chapter02/maps.ex
@@ -0,0 +1,8 @@
+# MAPS
+# A map is a key/value store, where keys and values can be any
+# term. Maps have dual usage in Elixir.
+# 1. They're used to power dynamically sized key/value structures
+# 2. They're used to manage simple records - a couple of well-defined named fields bundled together.
+#
+# DYNAMICALLY SIZED MAPS
+# An empty map can be created with the %{} construct:
