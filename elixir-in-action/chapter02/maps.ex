94d52aa5b9c47885f4fdb72b158387f17ddd3e13
mikeradka <mikeradka@gmail.com>
2023-10-27 20:18:19 -0400

    Add some notes

--- elixir-in-action/chapter02/maps.ex
+++ elixir-in-action/chapter02/maps.ex
@@ -6,3 +6,27 @@
 #
 # DYNAMICALLY SIZED MAPS
 # An empty map can be created with the %{} construct:
+#
+# iex(1)> empty_map = %{}
+# %{}
+#
+# A map with some values can be created this way:
+#
+# iex(1)> squares = %{1 => 1, 2 => 4, 3 => 9}
+# %{1 => 1, 2 => 4, 3 => 9}
+#
+# You can also prepopulate a map with the Map.new/1 function.
+# The function takes an enumerable where each element is a
+# tuple of size two (a pair):
+# iex(1)> squares = Map.new([{1, 1}, {2, 4}, {3, 9}])
+# %{1 => 1, 2 => 4, 3 => 9}
+#
+# To fetch a value at the given key, you can use the following:
+#
+# iex(2)> squares[2]
+# 4
+# iex(3)> squares[4]
+# nil
+#
+# In the second expression, squares[4], you get a nil because
+# no value is associated with the given key.
