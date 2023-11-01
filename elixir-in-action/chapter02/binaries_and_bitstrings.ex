d3eecfe054e268b2f458ff1068fc78a306f2238a
mikeradka <mikeradka@gmail.com>
2023-10-31 21:27:24 -0400

    Add some truncation examples

--- elixir-in-action/chapter02/binaries_and_bitstrings.ex
+++ elixir-in-action/chapter02/binaries_and_bitstrings.ex
@@ -1,3 +1,15 @@
+defmodule Print do
+  @moduledoc "Implements basic printing functions"
+
+  import IO
+
+  @doc "Prints the expression input and output with some padding"
+  def print_expression(input, output, padding) do
+    pad = String.pad_leading(" : ", padding)
+    puts(input <> pad <> to_string(output))
+  end
+end
+
 # BINARIES AND BITSTRINGS
 #
 # A binary is a chunk of bytes. You can create binaries by
@@ -6,7 +18,12 @@
 #
 # iex(1)> <<1, 2, 3>>
 # <<1, 2, 3>>
-#
+
+expr = <<1, 2, 3>>
+expr_str = "<<1, 2, 3>>"
+expr = inspect(expr)
+Print.print_expression(expr_str, expr, 0)
+
 # Each number represents the value of the corresponding byte.
 # If you provide a byte value bigger than 255, it's truncated
 # to the byte size:
@@ -17,3 +34,23 @@
 # <<1>>
 # iex(4)> <<512>>
 # <<0>>
+
+expr = <<255>>
+expr_str = "<<255>>"
+expr = inspect(expr)
+Print.print_expression(expr_str, expr, 7)
+
+expr = <<256>>
+expr_str = "<<256>>"
+expr = inspect(expr)
+Print.print_expression(expr_str, expr, 7)
+
+expr = <<257>>
+expr_str = "<<257>>"
+expr = inspect(expr)
+Print.print_expression(expr_str, expr, 7)
+
+expr = <<512>>
+expr_str = "<<512>>"
+expr = inspect(expr)
+Print.print_expression(expr_str, expr, 7)
