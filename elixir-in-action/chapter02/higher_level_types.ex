abb075a9383a9ae4124030e5dbcbb8795e608fd0
mikeradka <mikeradka@gmail.com>
2023-11-14 21:21:50 -0500

    Finish range type

--- elixir-in-action/chapter02/higher_level_types.ex
+++ elixir-in-action/chapter02/higher_level_types.ex
@@ -16,3 +16,31 @@ The ones most frequently used are:
   - DateTime
 "
 |> IO.puts()
+
+"
+---
+RANGE
+
+A range is an abstraction that can represent a range of numbers.
+Elixir has a special syntax to define ranges:
+
+  iex(1)> range = 1..2
+  1..2
+  iex(2)> 2 in range
+  true
+  iex(3)> -1 in range
+  false
+
+Ranges are enumerable, so functions from the Enum module can
+work with them. For example, to iterate through the range:
+
+  iex(4)> Enum.each(range, &IO.puts/1)
+  1
+  2
+  :ok
+
+A range is not a special type. It is represented as a map that
+contains range boundaries. So, the memory footprint of a range
+is very small. A million-number range is just a small map.
+"
+|> IO.puts()
