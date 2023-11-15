a157193f9063f78860cc70f247e06100ccc87081
mikeradka <mikeradka@gmail.com>
2023-11-14 21:11:38 -0500

    Add some notes

---
+++ elixir-in-action/chapter02/higher_level_types.ex
@@ -0,0 +1,18 @@
+"
+HIGHER-LEVEL TYPES
+
+Because Elixir code runs on BEAM, its type system is heavily
+influenced by Erlang's foundations. But on top of the basic
+foundational Erlang types, Elixir provides some higher-level
+abstractions.
+
+The ones most frequently used are:
+  - Range
+  - Keyword
+  - MapSet
+  - Date
+  - Time
+  - NaiveDateTime
+  - DateTime
+"
+|> IO.puts()
