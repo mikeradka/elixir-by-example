4fb6a75d72b9774d0bfcaef4fe7b82fceb847c73
mikeradka <mikeradka@gmail.com>
2023-11-21 17:37:17 -0500

    Start matching lists

--- elixir-in-action/chapter03/3.1.5_matching_lists.exs
+++ elixir-in-action/chapter03/3.1.5_matching_lists.exs
@@ -1,4 +1,16 @@
 "
 MATCHING LISTS
+
+List matching works similarly to tuples.
+This example decomposes a three-element list:
+
+  iex(1)> [first, second, third] = [1, 2, 3]
+  [1, 2, 3]
+  iex(2)> first
+  1
+  iex(3)> second
+  2
+  iex(4)> third
+  3
 "
 |> IO.puts()
