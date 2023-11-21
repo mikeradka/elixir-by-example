0d59351a307ffeba9b74a5aa457200fa0141355e
mikeradka <mikeradka@gmail.com>
2023-11-21 12:51:59 -0500

    Add some notes

--- elixir-in-action/chapter03/3.1.3_matching_constants.exs
+++ elixir-in-action/chapter03/3.1.3_matching_constants.exs
@@ -4,5 +4,11 @@ MATCHING CONSTANTS
 Matching is not confined to destructuring tuple elements to individual
 variables. Even constants are allowed on the left side of the match
 expression.
+
+  iex(1)> 1 = 1
+  1
+
+The match operator '=' tries to match the right-side term to the left-side
+pattern.
 "
 |> IO.puts()
