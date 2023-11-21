f500fe7cdcf1e01f0c754781ac90290e575fce74
mikeradka <mikeradka@gmail.com>
2023-11-20 21:05:27 -0500

    first matching tuple example

--- elixir-in-action/chapter03/3.1.2_matching_tuples.exs
+++ elixir-in-action/chapter03/3.1.2_matching_tuples.exs
@@ -14,5 +14,20 @@ to the corresponding elements of the tuple. To verify they are bound:
   \"Bob\"
   iex(3)> age
   25
+
+This feature is useful when you call a function that returns a tuple and
+you want to bind individual elements of that tuple to separate variables.
+For example, the Erlange function ':calendar.local_time/0' returns a tuple
+of the current date and time:
+
+  iex(1)> {date, time} = :calendar.local_time()
+  {{2023, 11, 20}, {21, 3, 56}}
+
+You now have separate variables for the date and time:
+
+  iex(2)> date
+  {2023, 11, 20}
+  iex(3)> time
+  {21, 3, 56}
 "
 |> IO.puts()
