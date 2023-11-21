64386e7cf2c18cd1c6ac88bec717c7a1388e7147
mikeradka <mikeradka@gmail.com>
2023-11-21 17:57:47 -0500

    head/tail pattern matching

--- elixir-in-action/chapter03/3.1.5_matching_lists.exs
+++ elixir-in-action/chapter03/3.1.5_matching_lists.exs
@@ -19,5 +19,17 @@ The previously mentioned pattern techniques also work:
   iex(2)> [first, first, first] = [1, 1, 1] # All elements must have the same value
   iex(3)> [first, second, _] = [1, 2, 3]    # You don't care about the third element, but it has to be there
   iex(4)> [^first, second, _] = [1, 2, 3]   # The first element must have the same value as the variable 'first'
+
+Matching lists is more often done by relying on their recursive nature. Remember,
+each non-empty list is a recurisve structure that can be expressed in the form
+of [head | tail]. You can use pattern matching to put each of these two
+elements into separate variables:
+
+  iex(1)> [head | tail] = [1, 2, 3]
+  [1, 2, 3]
+  iex(2)> head
+  1
+  iex(3)> tail
+  [2, 3]
 "
 |> IO.puts()
