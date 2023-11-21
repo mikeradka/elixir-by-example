fc196788101b798c9b44e30e849e15676fdc9641
mikeradka <mikeradka@gmail.com>
2023-11-21 18:07:25 -0500

    Show simple map matching example

--- elixir-in-action/chapter03/3.1.6_matching_maps.exs
+++ elixir-in-action/chapter03/3.1.6_matching_maps.exs
@@ -1,4 +1,13 @@
 "
 MATCHING MAPS
+
+To match a map, you can use the following syntax:
+
+  iex(1)> %{name: name, age: age} = %{name: \"Bob\", age: 25}
+  %{name: \"Bob\", age: 25}
+  iex(2)> name
+  \"Bob\"
+  iex(3)> age
+  25
 "
 |> IO.puts()
