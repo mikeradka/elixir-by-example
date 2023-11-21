97714e43c6693c61926c13e26892662b34e16c04
mikeradka <mikeradka@gmail.com>
2023-11-21 17:53:36 -0500

    Show some more list pattern matching techniques

--- elixir-in-action/chapter03/3.1.5_matching_lists.exs
+++ elixir-in-action/chapter03/3.1.5_matching_lists.exs
@@ -12,5 +12,12 @@ This example decomposes a three-element list:
   2
   iex(4)> third
   3
+
+The previously mentioned pattern techniques also work:
+
+  iex(1)> [1, second, third] = [1, 2, 3]    # The first element must be 1
+  iex(2)> [first, first, first] = [1, 1, 1] # All elements must have the same value
+  iex(3)> [first, second, _] = [1, 2, 3]    # You don't care about the third element, but it has to be there
+  iex(4)> [^first, second, _] = [1, 2, 3]   # The first element must have the same value as the variable 'first'
 "
 |> IO.puts()
