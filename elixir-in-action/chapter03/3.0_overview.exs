432e6c9456cd4eabb2d6866cac4a77b1e134282e
mikeradka <mikeradka@gmail.com>
2023-11-20 20:32:46 -0500

    Finish overview

---
+++ elixir-in-action/chapter03/3.0_overview.exs
@@ -0,0 +1,19 @@
+"
+CONTROL FLOW
+
+This chapter covers:
+  - Pattern matching
+  - Working with multiclause functions
+  - Using conditional expressions
+  - Working with loops
+
+Conditionals and Loops in Elixir work differently than in most modern languages.
+
+Classical conditional constructs such as 'if' and 'case' are often replaced
+with multiclause functions. There are also no classical loop statements such
+as 'while'. But, the resulting code is no more complicated.
+
+The underlying supporting mechanism for branching and looping is pattern
+matching, which is discussed first.
+"
+|> IO.puts()
--- elixir-in-action/chapter03/README.md
+++ elixir-in-action/chapter03/README.md
@@ -1,3 +1,5 @@
 # Chapter 3: Control Flow
 # TODO
 - [ ] 3.1.1 The match operator
+- [ ] 3.1.0 Pattern matching
+- [x] Overview (`3.0_overview.exs`)
