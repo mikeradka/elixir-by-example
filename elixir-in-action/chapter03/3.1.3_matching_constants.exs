2fb5c3f5eb701c943b1ab3c9a2bdd7e6149b0f80
mikeradka <mikeradka@gmail.com>
2023-11-21 12:55:42 -0500

    Add some notes

--- elixir-in-action/chapter03/3.1.3_matching_constants.exs
+++ elixir-in-action/chapter03/3.1.3_matching_constants.exs
@@ -9,6 +9,16 @@ expression.
   1
 
 The match operator '=' tries to match the right-side term to the left-side
-pattern.
+pattern. So in this example, you try to match the pattern 1 to the term 1.
+This succeeds (of course), and the result of the entire expression is the
+right-side term.
+
+Here is an example of a failure:
+
+  iex(2)> 2 = 1
+  ** (MatchError) no match of right hand side value: 1
+
+This example is only used to illustrate that you can place constants to the
+left of '=', proving that '=' is NOT an assignment operator.
 "
 |> IO.puts()
