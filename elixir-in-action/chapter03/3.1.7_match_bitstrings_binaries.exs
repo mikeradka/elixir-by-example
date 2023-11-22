7eb11f6b79c28c8fe15a0782aa29cdeba444411a
mikeradka <mikeradka@gmail.com>
2023-11-21 21:23:30 -0500

    Show a simple binary match

--- elixir-in-action/chapter03/3.1.7_match_bitstrings_binaries.exs
+++ elixir-in-action/chapter03/3.1.7_match_bitstrings_binaries.exs
@@ -1,4 +1,20 @@
 "
 MATCHING BITSTRINGS AND BINARIES
+
+A bitstring is a chunk of bits, and a binary is a special case of a bit-string
+that is always aligned to the byte size.
+
+To match a binary, you use a syntax similar to creating one:
+
+  iex(1)> binary = <<1, 2, 3>>
+  <<1, 2, 3>>
+  iex(2)> <<b1, b2, b3>> = binary   # A binary match
+  <<1, 2, 3>>
+  iex(3)> b1
+  1
+  iex(4)> b2
+  2
+  iex(5)> b3
+  3
 "
 |> IO.puts()
