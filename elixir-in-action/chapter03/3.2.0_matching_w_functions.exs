5453c7ea803bac5115552e3644cdbd386110504b
mikeradka <mikeradka@gmail.com>
2023-11-21 22:41:27 -0500

    Start Matching with Functions

--- elixir-in-action/chapter03/3.2.0_matching_w_functions.exs
+++ elixir-in-action/chapter03/3.2.0_matching_w_functions.exs
@@ -1,4 +1,14 @@
 "
 MATCHING WITH FUNCTIONS
+
+The pattern-matching mechanism is used in the specification of function
+arguments. Recall the basic function definition:
+
+  def my_function(arg1, arg2) do
+    ...
+  end
+
+The argument specifiers arg1 and arg2 are patterns, and you can use standard
+matching techniques.
 "
 |> IO.puts()
